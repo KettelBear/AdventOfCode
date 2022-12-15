dafdfad929f1158d0c9061787e310ceddd7aea9f
KettelBear <kttl_crn@hotmail.com>
2022-12-15 14:13:45 -0700

    2022 - Day 13 - Refactor

--- 2022/elixir/lib/advent/day/13/thirteen.ex
+++ 2022/elixir/lib/advent/day/13/thirteen.ex
@@ -1,7 +1,7 @@
 defmodule Advent.Day.Thirteen do
   @moduledoc false
 
-  defdelegate code(string), to: Code, as: :string_to_quoted!
+  defdelegate code!(string), to: Code, as: :string_to_quoted!
 
   alias Advent.Utility
 
@@ -12,14 +12,14 @@ defmodule Advent.Day.Thirteen do
   def part1 do
     "#{__DIR__}/input.prod"
     |> Utility.parse_input!(double: true)
-    |> Enum.map(fn [one, two] -> [code(one), code(two)] end)
+    |> Enum.map(fn [one, two] -> [code!(one), code!(two)] end)
     |> Enum.with_index(1)
     |> correct_order()
   end
 
   defp correct_order(signals) do
     Enum.reduce(signals, 0, fn {[one, two], idx}, acc ->
-      if correct?(one, two), do: acc + idx, else: acc
+      if comp(one, two) == :lt, do: acc + idx, else: acc
     end)
   end
 
@@ -31,9 +31,9 @@ defmodule Advent.Day.Thirteen do
     "#{__DIR__}/input.prod"
     |> Utility.parse_input!()
     |> Enum.reject(&(&1 == ""))
-    |> Enum.map(&Code.string_to_quoted!/1)
+    |> Enum.map(&code!/1)
     |> Enum.concat([[[2]], [[6]]])
-    |> Enum.sort(fn one, two -> correct?(one, two) end)
+    |> Enum.sort(fn one, two -> comp(one, two) == :lt end)
     |> Enum.with_index(1)
     |> Enum.reduce_while(1, &dividers/2)
   end
@@ -48,18 +48,23 @@ defmodule Advent.Day.Thirteen do
   #                            #
   ##############################
 
-  defp correct?([], []), do: nil
-  defp correct?([], _), do: true
-  defp correct?(_, []), do: false
-  defp correct?([one], [two]), do: correct?(one, two)
-  defp correct?(o, t) when is_integer(o) and is_list(t), do: correct?([o], t)
-  defp correct?(o, t) when is_list(o) and is_integer(t), do: correct?(o, [t])
-  defp correct?([o | one], [t | two]) do
-    case correct?(o, t) do
-      nil -> correct?(one, two)
-      boolean -> boolean
+  defp comp([], []), do: :eq
+  defp comp([], _), do: :lt
+  defp comp(_, []), do: :gt
+  defp comp([one], [two]), do: comp(one, two)
+  defp comp(o, t) when is_integer(o) and is_list(t), do: comp([o], t)
+  defp comp(o, t) when is_list(o) and is_integer(t), do: comp(o, [t])
+  defp comp([o | one], [t | two]) do
+    case comp(o, t) do
+      :eq -> comp(one, two)
+      comparator -> comparator
     end
   end
-  defp correct?(one, two), do: if one == two, do: nil, else: one < two
-
+  defp comp(one, two) do
+    cond do
+      one > two -> :gt
+      one < two -> :lt
+      true -> :eq
+    end
+  end
 end
