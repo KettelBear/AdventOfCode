0dd80e39a20dd780fcea322898e02a72258d9e0c
KettelBear <kettel.bear@gmail.com>
2023-09-06 11:53:12 -0400

    Update Test Text

--- 2022/elixir/test/advent_of_code_test.exs
+++ 2022/elixir/test/advent_of_code_test.exs
@@ -1,7 +1,7 @@
 defmodule AdventOfCodeTest do
   use ExUnit.Case
 
-  test "greets Advent of Code, 2022" do
+  test "Greets Advent of Code, 2022" do
     assert AdventOfCode2022.hello() == :advent
   end
 end
